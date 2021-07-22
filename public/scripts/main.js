08f22d56dcdc6403284c3619ef6a63adfbabd231
QueirozVinicius <felipevs.queiroz@gmail.com>
2021-07-22 15:43:08 -0300

    :rocket: Finished the guided project.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -39,7 +39,7 @@ function handleClick(event, check = true){
     const roomId = document.querySelector("#room-id").dataset.id 
 
     const form = document.querySelector('.modal form')
-    form.setAttribute('action', `/room/${roomId}/${questionId}/${slug}`)
+    form.setAttribute('action', `/question/${roomId}/${questionId}/${slug}`)
 
     modalTitle.innerHTML =  `${text} esta pergunta`
     modalDescription.innerHTML = `Tem certeza de que você deseja ${text.toLowerCase()} esta pergunta?`
--- src/route.js
+++ src/route.js
@@ -6,13 +6,15 @@ const route = express.Router()
 
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
 route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
+route.post('/enterroom', RoomController.enter)
 
-
-route.get('/room/:room', (req, res) => res.render("room"))
 
 //Formato que o form dentro da modal tem que passar a informação
-route.post('/room/:room/:question/:action', QuestionController.index)
-route.post('/room/create-room', RoomController.create)
+route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
 
 
 module.exports = route
